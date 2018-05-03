7b2e5290c67aeff902155f0f2e37b603c0f44506
Theo <theokobb@gmail.com>
2018-05-03 15:38:10 -0400

    Implement popover

--- demo/src/App/components/PopoverDemo.jsx
+++ demo/src/App/components/PopoverDemo.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
 import Popover from '../../../../src/Popover'
+import styled from 'styled-components'
 
-export default ({}) => {
-    return <Popover />
+const Container = styled.main`
+    background-color: coral;
+    display: inline-block;
+`
+
+export default class PopoverDemo extends React.Component{
+    constructor(props){
+        super(props)
+        
+    }
+    render(){
+        return <div>
+            <Popover 
+                position={{
+                    left: '100%',
+                    top: 0,
+                    bottom: 0
+                }}
+                OpenComponent={() => <button>close</button>}
+                ClosedComponent={() => <button>open</button>}
+            >
+                <Container>
+                    <p>hi</p>
+                    <p>bye</p>
+                    <p>bonjour</p>
+                    <p>aurevoir</p>
+                    <p>hallo</p>
+                </Container>
+            </Popover >
+        </div>
+    }
 }
---
+++ src/Popover/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const OuterContainer = styled.div`
+    position: relative;
+    display: inline-block;
+`
+const InnerContainer = styled.div`
+    position: absolute;
+`
+
+export default class Popover extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+    validatePosition(position){
+        const validPositions = ['top', 'right', 'bottom', 'left']
+        Object.keys(position).forEach(position => {
+            if(!validPositions.includes(position)){
+                console.warn(`Invalid position in oui.Popover: ${position}`)
+            }
+        })
+    }
+    render(){
+        const { OpenComponent, ClosedComponent } = this.props
+        if(this.state.isOpen){
+            return <OuterContainer>
+                <div onClick={e => this.setState({ isOpen: false })}>
+                    <OpenComponent />
+                </div>
+                <InnerContainer
+                    style={this.props.position}
+                >
+                    {this.props.children}
+                </InnerContainer>
+            </OuterContainer>
+        }
+
+        return <div>
+            <div onClick={e => this.setState({ isOpen: true })}>
+                <ClosedComponent/>
+            </div>
+        </div>
+    }
+}
