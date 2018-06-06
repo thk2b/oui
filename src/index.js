16f30a4dbbd4d08db8a777c8c01f757cd8dbfea5
Theo <theokobb@gmail.com>
2018-06-06 11:06:49 -0400

    Export Editable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Grid } from './Grid'
 export { default as Resizable } from './Resizable'
 export { default as Popover } from './Popover'
+export { default as Editable } from './Editable'
